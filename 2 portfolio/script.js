2484aa8bc690aaf1989fc6e6a0996a211b45a8bb
kan <107451805+farj123@users.noreply.github.com>
2023-07-23 17:03:16 +0530

    Add files via upload

---
+++ 2 portfolio/script.js
@@ -0,0 +1,20 @@
+// console.log("Are you a ?");
+// document.querySelector('.cross').style.display = 'none';
+// document.querySelector('.hamburger').addEventListener("click" , ()=>{
+//     document.querySelector('.sidebar').classList.toggle('sidebarGo');
+//     document.querySelector('.cross').style.display = 'unhide';
+//     if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+//         document.querySelector('.cross').style.display = 'none';
+//         setTimeout(()=>{
+//             document.querySelector('.ham').style.display = 'inline';
+//     } , 320);
+
+//     }
+//     else{
+//         document.querySelector('.ham').style.display = 'none';
+//         setTimeout(()=>{
+//             document.querySelector('.cross').style.display = 'inline';
+//         } , 320);
+//     }
+
+// })
